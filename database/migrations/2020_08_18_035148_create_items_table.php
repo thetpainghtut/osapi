e9fe9cdfe4060aba24ea7f8e98e491055064fb94
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-08-29 00:44:07 +0630

    update migration for items charset

--- database/migrations/2020_08_18_035148_create_items_table.php
+++ database/migrations/2020_08_18_035148_create_items_table.php
@@ -14,6 +14,9 @@ class CreateItemsTable extends Migration
     public function up()
     {
         Schema::create('items', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_bin';
+            
             $table->id();
             $table->string('codeno');
             $table->string('name');
