87f70f57712b5d011aadd283ec13e79329c0afd5
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-09-05 19:33:24 +0630

    call order resource in order controller

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Order;
 use Illuminate\Http\Request;
+use App\Http\Resources\OrderResource;
 
 class OrderController extends Controller
 {
