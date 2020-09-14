d33cce90e7dfe8c0fc3e6cfd1be293f3e488955d
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-09-15 00:28:40 +0630

    set timezone and response datetime

--- app/Http/Resources/ItemResource.php
+++ app/Http/Resources/ItemResource.php
@@ -35,8 +35,8 @@ class ItemResource extends JsonResource
             'qty' => $this->whenPivotLoaded('order_detail', function () {
                 return $this->pivot->qty;
             }),
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
     }
 }
